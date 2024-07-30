a16b6869159e64417a434d20b4def53194a2518c
root <root@shicaolong.kuajingdc.com>
2024-07-30 20:26:14 +0800

    update

--- src/main/java/com/shicaolong/function/FunctionApplication.java
+++ src/main/java/com/shicaolong/function/FunctionApplication.java
@@ -2,6 +2,7 @@ package com.shicaolong.function;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /*
 1. @SpringBootConfiguration
@@ -13,6 +14,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 	@ComponentScan 告诉Spring在当前包及其子包中查找组件、配置和服务。这样可以发现并注册被@Component, @Service, @Repository, @Controller等注解标注的类。
  */
 @SpringBootApplication
+//@ComponentScan("com.shicaolong.function")
 public class FunctionApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/shicaolong/function/dao/IndexDao.java
+++ src/main/java/com/shicaolong/function/dao/IndexDao.java
@@ -1,10 +1,12 @@
 package com.shicaolong.function.dao;
 
 import com.shicaolong.function.pojo.Function;
+import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Mapper
 @Repository
 public interface IndexDao {
     public List<Function> getAllFunction();
